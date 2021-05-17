b4d6b532549da935d46ed819beba532d162bdeef
Alex Rauenzahn <xandermv@yahoo.com>
2021-05-17 00:25:44 +0000

    Adding prev to LL

--- Main.java
+++ Main.java
@@ -2,16 +2,14 @@ import java.util.LinkedList;
 
 class Main {
   public static void main(String[] args) {
-   LinkedList<Integer> numbers = new LinkedList<>();
-   numbers.add(10);
-   numbers.add(12);
-   numbers.add(45);
-   numbers.add(87);
-   numbers.add(99);
-
-   for(int i = 0; i < numbers.size(); i++){
-     System.out.println(numbers.get(i) + " ");
-   }
+  
   }
-
+class Node{
+  int data;
+  Node next;
+  Node prev;
+  Node(int data){
+    this.data = data;
+  }
+}
 }
